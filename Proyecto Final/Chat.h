75f6049baea99d6f486368af52238304856f89f4
Alejandro Placer <aplazer@ucm.es>
2020-05-31 18:44:36 +0200

    Corregido

--- Proyecto Final/Chat.h
+++ Proyecto Final/Chat.h
@@ -1,145 +1,97 @@
-#include "Chat.h"
-#include <algorithm>
 #include <string>
+#include <unistd.h>
 #include <string.h>
-#include "XLDisplay.h"
+#include <vector>
+#include "Serializable.h"
+#include "Socket.h"
 
-void ChatMessage::to_bin()
-{
-    alloc_data(MESSAGE_SIZE);
-    memset(_data, 0, MESSAGE_SIZE);
-	char* dt = _data;
+class Game : public Serializable {
+public:
+    static const size_t SIZE = sizeof(uint8_t) * 4;
+    uint8_t x1;
+    uint8_t y1;
+    int x2;
+    int y2;
 	
-    memcpy(dt, &type, sizeof(uint8_t));
-	dt += sizeof(uint8_t);
+    Game(uint8_t _x1, uint8_t _y1, uint8_t _x2, uint8_t _y2) {
+        x1 = _x1;
+        y1 = _y1;
+        x2 = _x2;
+        y2 = _y2;
+        std::cout << x1 << "\n";
+    };
 	
-	for (unsigned int i = 0; i < 7 && i < nick.length(); i++, dt++) {
-	 *dt = nick[i];
-	}
-	dt += 7 * sizeof(char);
-	
-	for (unsigned int i = 0; i < message.length(); i++, dt++) {
-	 *dt = message[i];
-	}
-	dt = _data + sizeof(uint8_t) + 8* sizeof(char);
-}
-
-int ChatMessage::from_bin(char * bobj)
-{
-    alloc_data(MESSAGE_SIZE);
-    memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
-	type = (uint8_t) *_data;
-	
-	char * _nick = _data + sizeof(uint8_t);
-	char * _msg = _nick + sizeof(char) * 8;
-	
-	std::string n(_nick, 8);
-	std::string m(_msg, 80);
-	
-	message = m;
-	nick = n;
-	
-    return 0;
-}
-
-void ChatServer::do_messages()
-{
-    while (true)
-    {
-	ChatMessage message;
-	Socket* sdMessage;
-	socket.recv(message, sdMessage);
-	
-	switch(message.type) {
-		case ChatMessage::LOGIN:
-			clients.push_back(sdMessage);
-			std::cout << "login: " << *sdMessage << "\n";
-		break;
-		case ChatMessage::MESSAGE:
-			for (auto it = clients.begin(); it != clients.end(); ++it) {
-				if(!(*(*it) == *sdMessage)) {
-					socket.send(*game, *(*it));
-				}
-			}
-			std::cout << "message from: " << *sdMessage << "\n";
-		break;
-		case ChatMessage::LOGOUT:
-			int i;
-			i = 0;
-			bool encontrado;
-			encontrado = false;
-			
-			while (!encontrado && i != clients.size()) {
-				if (clients[i] == sdMessage) {
-					encontrado = true;
-					clients.erase(clients.begin() + i);
-				}
-				else i++;
-			}
-			std::cout << "logout: " << *sdMessage << "\n";
-		break;
-		}
+    void to_bin() {
+        alloc_data(sizeof(uint8_t));
+        memset(_data, 0, sizeof(uint8_t));
+        char* dt = _data;
+        memcpy(dt, &x1, sizeof(uint8_t));
+        dt += sizeof(uint8_t);
+        dt = _data + (sizeof(uint8_t));
     }
-}
 
-void ChatClient::login()
-{
-    std::string msg;
-    ChatMessage em(nick, msg);
-    em.type = ChatMessage::LOGIN;
-    socket.send(em, socket);
-}
+    int from_bin(char * bobj) {
+        alloc_data(sizeof(uint8_t));
+        std::cout << x1 << "\n";
+        memcpy(static_cast<void *>(_data), bobj, sizeof(uint8_t));
+        x1 = (uint8_t) *_data;
+        std::cout << x1 << "\n";
+        return 0;
+    };
+};
 
-void ChatClient::logout()
+class ChatMessage: public Serializable
 {
-	std::string msg;
-    ChatMessage em(nick, msg);
-    em.type = ChatMessage::LOGOUT;
-    socket.send(em, socket);
-}
-
-void ChatClient::input_thread()
-{
-    while (true)
+public:
+	static const size_t MESSAGE_SIZE = sizeof(char) * 88 + sizeof(uint8_t);
+	
+    enum MessageType
     {
-		std::string msg;
-		std::getline(std::cin, msg);
-		if (msg == "q") {
-			ChatMessage em(nick, msg);
-			em.type = ChatMessage::LOGOUT;
+        LOGIN   = 0,
+        MESSAGE = 1,
+        LOGOUT  = 2
+    };
+
+    ChatMessage(){};
+    ChatMessage(const std::string& n, const std::string& m):nick(n),message(m){};
+	
+    void to_bin();
+    int from_bin(char * bobj);
+    
+	uint8_t type;
+    std::string nick;
+    std::string message;
+};
+
+class ChatServer
+{
+public:
+    ChatServer(const char * s, const char * p): socket(s, p)
+    {
+        socket.bind();
+        game = new Game(200, 50, 125, 200);
+    };
+	
+    void do_messages();
+	
+private:
+    std::vector<Socket *> clients;
+    Socket socket;
+    Game* game;
+};
+
+class ChatClient
+{
+public:
+    ChatClient(const char * s, const char * p, const char * n):socket(s, p),
+        nick(n){};
 		
-			socket.send(em, socket);
-			break;
-		}
-		else {
-			ChatMessage em(nick, msg);
-			em.type = ChatMessage::MESSAGE;
-			
-			socket.send(em, socket);
-		}
-    }
-}
-
-void ChatClient::net_thread()
-{
-    while(true)
-    {
-        //Recibir Mensajes de red
-        //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
-		/*ChatMessage em;
-		socket.recv(em);		
-		std::cout << em.nick << ": " << em.message << "\n";*/
-		Game game(0, 0, 240, 240);
-		socket.recv(game);
-		//std::cout << game.x1 << " " 
-		//		  << game.y1 << " " 
-		//		  << game.x2 << " " 
-		//		  << game.y2 << "\n";
-		while (true) {
-			XLDisplay& dpy = XLDisplay::display();
-			dpy.set_color(XLDisplay::BLUE);
-			dpy.circle(game.x1, game.y1, 20);
-			dpy.circle(game.x2, game.y2, 20);
-		}
-    }
-}
+    void login();
+    void logout();
+    void input_thread();
+    void net_thread();
+	
+private:
+    Socket socket;
+    std::string nick;
+};
